4655eabd42e21a59d0c08849482643e0e5f5e831
manja1227 <manjunatha1227@gmail.com>
2023-08-24 00:23:37 +0530

    init auto files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  images: {
+    loader: 'default',
+    domains: ['localhost', 'vision-creations-vmgfq.ondigitalocean.app', 'mdbcdn.b-cdn.net'],
+  },
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+});
+
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
+    safelist: ['animate-[fade-in_1s_ease-in-out]', 'animate-[fade-in-down_1s_ease-in-out]', 'animate-[fade-in_2s_ease-in-out]', 'animate-[fade-in-down_2s_ease-in-out]'],
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('tw-elements/dist/plugin.cjs')],
+};
+export default config;
