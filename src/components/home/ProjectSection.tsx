7ba9d0d0ec9eaf34a3c4596159d2fd3903f2eb91
manja1227 <manjunatha1227@gmail.com>
2023-08-30 09:31:49 +0530

    fix project layout

--- src/components/home/ProjectSection.tsx
+++ src/components/home/ProjectSection.tsx
@@ -11,7 +11,7 @@ const ProjectSection = async () => {
     const slug = `${item.attributes.name.toLowerCase().replace(' ', '-')}-at-${item.attributes.location.city
       .toLowerCase()
       .replace(' ', '-')}`;
-    const classes = i % 2 ? 'w-full md:w-3/5 mt-10s md:mt-10s' : 'w-full md:w-1/3 mt-4s md:-mt-24s h-fit';
+    const classes = i % 2 ? 'w-full md:w-3/5 mt-10s md:ml-[40%] md:-mt-36' : 'w-full md:w-1/3 mt-4s md:-mt-24s h-fit';
     return (
       <figure key={i} className={classes}>
         <Link href={`/projects/${item.id}/${slug}`}>
@@ -34,10 +34,10 @@ const ProjectSection = async () => {
 
   return (
     <section className='w-4/5 mx-auto my-6 md:my-32'>
-      <h2 className='text-3xl md:text-4xl lg:text-4xl font-bold mt-36 md:mb-32 text-center md:text-left'>
+      <h2 className='text-3xl md:text-4xl lg:text-4xl font-bold my-10 md:mb-32 text-center md:text-left'>
         Our Projects
       </h2>
-      <div className='row flex flex-col md:flex-rows justify-between mb-4'>{featured}</div>
+      <div className='row flex flex-col md:flex-rows justify-between my-4 gap-10'>{featured}</div>
       <div className='mt-4 text-right'>
         <Link className='text-base font-semibold hover:underline hover:underline-offset-4' href='/projects'>
           see all
